d0723355aa7206f41cfed88cbb3fc7f29633f4d9
Davis Gramza <davisgramza@gmail.com>
2020-04-23 22:48:24 -0500

    Added support for user-supplied database URL
    Signed-off-by: Davis Gramza <davisgramza@gmail.com>

--- app.py
+++ app.py
@@ -10,7 +10,10 @@ from flask_sqlalchemy import SQLAlchemy
 from messages import Errors
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + os.getenv('DATABASE_USERNAME', default='admin') + ':' + os.getenv('DATABASE_PASSWORD', default='password') + '@' + os.getenv('DATABASE_SERVER', default='localhost') + os.getenv('DATABASE_PORT', default='3306') + '/' + os.getenv('DATABASE_NAME')
+if os.environ.get('DATABASE_URL'):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + os.getenv('DATABASE_USERNAME', default='admin') + ':' + os.getenv('DATABASE_PASSWORD', default='password') + '@' + os.getenv('DATABASE_SERVER', default='localhost') + os.getenv('DATABASE_PORT', default='3306') + '/' + os.getenv('DATABASE_NAME')
 app.secret_key = os.getenv('SECRET_KEY', default='8648d22e542ca1188d20de52b52c6d33d209704f508f21fc')
 app.config['MAIL_SERVER'] = os.getenv('PROGRAM_EMAIL_SERVER')
 app.config['MAIL_USERNAME'] = os.getenv('PROGRAM_EMAIL_ADDRESS')
